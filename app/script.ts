d300f81ce72e1b0513ce6a44fb2d7f0a34a2ee77
Hironao OTSUBO <motemen@gmail.com>
2022-07-15 23:55:45 +0900

    wi

--- app/script.ts
+++ app/script.ts
@@ -5,72 +5,126 @@ interface Cut {
   end?: number;
 }
 
+let canvas: HTMLCanvasElement | undefined;
 function measureTextWidth(text: string, element: Element): number {
-  const canvas = document.createElement("canvas");
+  if (!canvas) {
+    canvas = document.createElement("canvas");
+  }
   canvas.style.font = getComputedStyle(element).font;
   return canvas.getContext("2d")!.measureText(text).width;
 }
 
-function obfuscateText(text: string, cut: Cut = {}): string {
-  console.debug({ text, cut });
+const ZERO_WIDTH_SPACE = "\u200B";
+
+function obfuscateText(text: string): string {
   return [...text]
     .map((char, i) => {
-      if (i < (cut.start ?? 0)) {
-        return char;
-      }
-      if (i >= (cut.end ?? text.length)) {
-        return char;
-      }
       if (/^\s+$/.test(char)) {
         return char;
       }
-      return OBFSCUSED_CHARS[
-        Math.floor(Math.random() * OBFSCUSED_CHARS.length)
-      ];
+      return (
+        OBFSCUSED_CHARS[Math.floor(Math.random() * OBFSCUSED_CHARS.length)] +
+        ZERO_WIDTH_SPACE
+      );
     })
     .join("");
 }
 
-function obfuscateNode(node: Node, cut?: Cut) {
-  if (node.nodeType === node.TEXT_NODE) {
-    node.textContent = obfuscateText(node.textContent ?? "", cut);
-  } else {
-    [...node.childNodes].forEach((child) => {
-      obfuscateNode(child);
-    });
-  }
+function cutText(text: string, cut?: Cut): [string, string, string] {
+  const start = cut?.start ?? 0;
+  const end = cut?.end ?? text.length;
+
+  return [
+    text.substring(0, start),
+    text.substring(start, end),
+    text.substring(end),
+  ];
 }
 
-function obfuscateRange(selection: Selection, range: Range) {
-  const it = document.createNodeIterator(
-    range.commonAncestorContainer,
-    NodeFilter.SHOW_TEXT
-  );
-
-  for (let node = it.nextNode(); node; node = it.nextNode()) {
-    if (!selection.containsNode(node, true)) {
-      continue;
-    }
-    let start, end: number | undefined;
-    if (node === range.startContainer) {
-      start = range.startOffset;
-    }
-    if (node === range.endContainer) {
-      end = range.endOffset;
-    }
-    obfuscateNode(node, { start, end });
+function obfuscateTextNode(node: Node, cut?: Cut) {
+  console.debug("obfuscateTextNode", { node, cut });
+  if (node.nodeType !== node.TEXT_NODE) {
+    throw new Error("node is not a text node");
   }
+
+  const nodeText = node.textContent;
+  if (!nodeText) {
+    return;
+  }
+  const [pre, text, post] = cutText(nodeText, cut);
+  let obfuscatedText = obfuscateText(text);
+
+  const originalWidth = measureTextWidth(text, node.parentElement!);
+  const obfuscatedWidth = measureTextWidth(obfuscatedText, node.parentElement!);
+
+  console.debug({ originalWidth, obfuscatedWidth });
+
+  if (obfuscatedWidth > originalWidth) {
+    obfuscatedText = obfuscatedText.substring(
+      0,
+      (obfuscatedText.length * originalWidth) / obfuscatedWidth
+    );
+  }
+
+  node.textContent = `${pre}${obfuscatedText}${post}`;
 }
 
-function obfuscateSelection(selection: Selection) {
-  for (let i = 0; i < selection.rangeCount; i++) {
-    obfuscateRange(selection, selection.getRangeAt(i));
+function nodeCutFromRange(range: Range, node: Node): Cut {
+  const start = range.startContainer === node ? range.startOffset : undefined;
+  const end = range.endContainer === node ? range.endOffset : undefined;
+  return { start, end };
+}
+
+function textNodesFromRange(range: Range): Node[] {
+  let results = <Node[]>[];
+  let node: Node | null = range.startContainer;
+  while (node) {
+    let atEnd: boolean = false;
+    if (node.nodeType === node.TEXT_NODE) {
+      results.push(node);
+      atEnd = node === range.endContainer;
+    } else if (node.nodeType === node.ELEMENT_NODE) {
+      const visit = (node: Node): boolean => {
+        if (node.nodeType === node.TEXT_NODE) {
+          results.push(node);
+        } else if (node.nodeType === node.ELEMENT_NODE) {
+          const { start, end } = nodeCutFromRange(range, node);
+          const atEnd = [...node.childNodes].slice(start, end).some((child) => {
+            return visit(child);
+          });
+          if (atEnd) return true;
+        }
+        return node === range.endContainer;
+      };
+      atEnd = visit(node);
+    }
+
+    if (atEnd) {
+      return results;
+    }
+
+    while (!node.nextSibling) {
+      node = node.parentElement;
+      if (!node || node === range.commonAncestorContainer) {
+        return results;
+      }
+    }
+    node = node?.nextSibling;
   }
+  return results;
 }
 
 const selection = document.getSelection();
 if (selection && selection.isCollapsed === false) {
-  obfuscateSelection(selection);
+  for (let i = 0; i < selection.rangeCount; i++) {
+    const range = selection.getRangeAt(i);
+    textNodesFromRange(range).forEach((node) => {
+      obfuscateTextNode(node, nodeCutFromRange(range, node));
+    });
+  }
 } else {
-  obfuscateNode(document.body);
+  const it = document.createNodeIterator(document.body, NodeFilter.SHOW_TEXT);
+  for (let node = it.nextNode(); node; node = it.nextNode()) {
+    obfuscateTextNode(node);
+  }
 }
